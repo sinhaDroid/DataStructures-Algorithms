29ae59d78fa4bd31dcff7605ccdc388608caf244
Deepanshu Sinha <107sinha@gmail.com>
2018-07-02 09:37:12 +0530

    corrected indent spaces in question 1

--- Assignment - 1/question1.py
+++ Assignment - 1/question1.py
@@ -1,3 +1,10 @@
+"""
+    Given two strings s and t, determine whether some anagram of t 
+    is a substring of s. For example: if s = "udacity" and t = "ad", 
+    then the function returns True. Your function definition should 
+    look like: question1(s, t) and return a boolean True or False.
+"""
+
 
 def question1(s, t):
 
@@ -10,12 +17,12 @@ def question1(s, t):
 
     for x in range(s_len - t_len + 1):
         temp = s[x: x + t_len]
-        print sorted(temp)
 
         # match with sorted value of temp with sorted value of t
         if t_sort == sorted(temp):
             return True
-    
+
     return False
+
 
 print(question1("udacity", "ad"))
