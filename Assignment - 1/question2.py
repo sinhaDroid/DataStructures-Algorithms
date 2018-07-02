7c5e5020119622911eb647a1959f9b7e01789e27
Deepanshu Sinha <107sinha@gmail.com>
2018-07-02 22:29:12 +0530

    question2 has been modified

--- Assignment - 1/question2.py
+++ Assignment - 1/question2.py
@@ -1,7 +1,21 @@
+"""
+    Given a string a, find the longest palindromic substring contained in a. 
+    Your function definition should look like question2(a), and return a string.
+"""
+
 
 def question2(a):
+    # make sure a is a string
+    if type(a) != str:
+        return "Error: 'a' is not a string"
+
+    # make sure 'a' has atleast 2 characters
+    if len(a) < 2:
+        return a
+    
     # convert string to lowercase
     text = a.lower()
+
     # append result here
     results = []
 
@@ -12,10 +26,16 @@ def question2(a):
             # if palindromic string found, append in results
             if temp == temp[::-1]:
                 results.append(temp)
-    
+
     # return max string with index
-    return text.index(max(results, key=len)), results
+    return results
 
 
-# Should print (1, ['ala'])
+print(question2(3))
+# Should print - Error: 'a' is not a string
+
+print(question2("aa"))
+# Should print - ['aa']
+
 print(question2("palaindromeasfudacity"))
+# Should print (1, ['ala'])
