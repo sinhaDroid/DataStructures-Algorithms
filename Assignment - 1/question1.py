bf04695efa6061d983c6a6edfd5d8228be7fbed1
Deepanshu Sinha <107sinha@gmail.com>
2018-07-08 22:38:23 +0530

    question1 explanations

--- Assignment - 1/question1.py
+++ Assignment - 1/question1.py
@@ -50,3 +50,23 @@ print(question1("ad", "udacity"))
 
 print(question1("udacity", "ad"))
 # It should print - true
+
+
+# Explanations
+
+"""
+    The two strings are anagram by checking and comparing all character of 
+    one string to be present as a substring of another string. This should only
+    accept two strings and if one of the string is empty, then both string will 
+    always be anagram. For 's' and 't' to be anagram, 's'must have at least as much
+    character as 't'. We will split and sort the character of 't' using sorted function
+    and store it in temporary variable. We will check every possible consecutive substring
+    in 's' with string 't'. If any set is anagram of 't', then we return True, else False.
+    Comparing counts of all characters will be done in constant time since there are 
+    only limited amount of characters to check. Looping through all possible consecutive 
+    substrings will take worst case O(len(s)). 
+    Therefore, the time complexity of this algorithm is O(len(s)).
+    Since, the number of characters in 't' and 's' are bounded, the space used to load 't' 
+    and 's' is 0(1).
+    Therefore, the space complexity of this algorithm is O(1).
+"""
