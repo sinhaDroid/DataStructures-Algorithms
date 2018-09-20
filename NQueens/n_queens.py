6e8d4611d73cc759a08613acc9cbce49310cef8f
Deepanshu Sinha <107sinha@gmail.com>
2018-09-20 19:35:10 +0530

    git ignore files added

--- NQueens/n_queens.py
+++ NQueens/n_queens.py
@@ -9,13 +9,15 @@ def initialize(n):
 
     for i in range(-(n-1), n):
         board['nwtose'][i] = 0
+
     for i in range(2*n-1):
         board['swtone'][i] = 0
 
 
 def printboard():
     for row in sorted(board['queen'].keys()):
-        print((row, board['queen'][row]))
+        print((row, board['queen'][row]), end=" ")
+    print("")
 
 
 def free(i, j):
